f25f94773d7124e2252deff2572f38c5fce06061
Billy Hawkes <billyhawkes02@gmail.com>
2025-04-04 14:06:20 -0400

    refactor: clean up middleware

--- app.config.ts
+++ app.config.ts
@@ -2,19 +2,10 @@
 import { defineConfig } from "@tanstack/react-start/config";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { Resource } from "sst";
 
 export default defineConfig({
 	server: {
 		preset: "bun",
-		//routeRules: {
-		//	// Move to tss rewrite when available
-		//	"/cdn/**": {
-		//		proxy: {
-		//			to: `https://${Resource.Bucket.name}.s3.amazonaws.com/**`,
-		//		},
-		//	},
-		//},
 		rollupConfig: {
 			output: {
 				format: "esm",
@@ -27,12 +18,6 @@ export default defineConfig({
 		},
 	},
 	routers: {
-		api: {
-			middleware: "./middleware.ts",
-		},
-		server: {
-			middleware: "./middleware.ts",
-		},
 		ssr: {
 			middleware: "./middleware.ts",
 		},
