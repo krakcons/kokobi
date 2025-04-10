13d325cdb491c46567bf189c775f0bc0bebf14ac
Billy Hawkes <billyhawkes02@gmail.com>
2025-04-10 11:21:36 -0400

    fix: loader types

--- app/router.tsx
+++ app/router.tsx
@@ -37,6 +37,6 @@ export const createRouter = () => {
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
 	interface Register {
-		router: typeof createRouter;
+		router: ReturnType<typeof createRouter>;
 	}
 }
