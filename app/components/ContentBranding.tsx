17acfec45dff34fa64c023e27dbab5f4ce8fc39a
Billy Hawkes <billyhawkes02@gmail.com>
2025-04-14 14:09:54 -0400

    fix: content branding gap

--- app/components/ContentBranding.tsx
+++ app/components/ContentBranding.tsx
@@ -10,22 +10,24 @@ export const ContentBranding = ({
 	connectTeam: Team & TeamTranslation;
 }) => {
 	return (
-		<div className="flex gap-2 items-center">
+		<div className="flex items-center">
 			<TeamAvatar>
 				<TeamAvatarImage
 					src={`${env.VITE_SITE_URL}/cdn/${connectTeam.id}/${connectTeam.locale}/logo?updatedAt=${connectTeam.updatedAt}`}
 					alt="Delivered by team logo"
-					className="max-h-10"
+					className="max-h-10 mr-2"
 				/>
 			</TeamAvatar>
-			<p>
-				Delivered by <strong>{connectTeam.name}</strong>
-			</p>
-			{team.id !== connectTeam.id && (
-				<p className="text-sm text-muted-foreground">
-					Created by <strong>{team.name}</strong>
+			<div className="flex gap-2 items-center">
+				<p>
+					Delivered by <strong>{connectTeam.name}</strong>
 				</p>
-			)}
+				{team.id !== connectTeam.id && (
+					<p className="text-sm text-muted-foreground">
+						Created by <strong>{team.name}</strong>
+					</p>
+				)}
+			</div>
 		</div>
 	);
 };
