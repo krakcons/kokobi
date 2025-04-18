792ff1378536cd29eb85777053ef179d7656a0d7
Billy Hawkes <billyhawkes02@gmail.com>
2025-04-18 09:50:47 -0400

    fix: font loading shift by moving to head

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -71,6 +71,18 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 			],
 			links: [
 				{
+					rel: "preconnect",
+					href: "https://fonts.googleapis.com",
+				},
+				{
+					rel: "preconnect",
+					href: "https://fonts.gstatic.com",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+				},
+				{
 					rel: "stylesheet",
 					href: appCss,
 				},
